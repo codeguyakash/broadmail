52e7af036951ec0f07511b04f4555591474df292
Akash (codeguyakash) <codeguyakash@gmail.com>
2024-03-13 04:51:57 +0530

    Update favicon link in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="icon" href="./favicon.svg" sizes="any" />
+      <link rel="icon" href="https://nextjs.org/static/favicon/apple-touch-icon.png" sizes="any" />
       <body className={inter.className}>
         <Provider>{children}</Provider>
       </body>
